f6520716e6887d074bf054a319a8f5bf13287a3c
mgua <mgua@tomware.it>
2023-10-10 12:50:08 +0200

    removed activate.bat added pinstall alias

--- profile.ps1
+++ profile.ps1
@@ -2,7 +2,11 @@
 # mgua@tomware.it
 # october 2023
 #
-# save it in C:\Users\<username>\Documents\profile.ps1 
+# save it in the file name specified by the $PROFILE variable
+# if the file does not exist, we need to create it
+#	New-Item -ItemType File -Path $PROFILE -Force
+#
+# C:\Users\<username>\Documents\profile.ps1 
 #	this has to be the path pointed by the $profile variable 
 # and could be different, like
 #	c:\Users\<username>\Onedrive\Documents\...
@@ -21,6 +25,32 @@
 # check last access time of a folder/file
 # Get-ChildItem | Where-Object {$_.psiscontainer} | ForEach-Object {“{0}`t{1}” -f $_.name,$_.lastaccesstime}
 #
+
+function Profile-Install {
+	# this file will have to be appended to the existing $PROFILE file
+	#
+	$currentFolder = Get-Location
+	$newProfile = Join-Path $currentFolder "profile.ps1"
+	if (Test-Path $newProfile) {
+		Write-Host "newProfile found: [$newProfile]"
+		if (Test-Path $PROFILE) {
+			Write-Host "PROFILE: [$PROFILE] exists"
+		} else {
+			New-Item -ItemType File -Path $PROFILE -Force
+			Write-Host "PROFILE: [$PROFILE] created"
+		}
+		# $thisScriptName = $MyInvocation.MyCommand.Name
+		#$thisScriptPath = $PSScriptRoot
+		#$thisScriptFullName = Join-Path $thisScriptPath $thisScriptName
+		Write-Host "copy/append newProfile to PROFILE:"
+		Write-Host "  copy `"$newProfile`" `"$PROFILE`""
+	} else {
+		Write-Host "newProfile not found: [$newProfile]"
+		Write-Host "currentFolder: [$currentFolder] expected newProfile: [$newProfile]"
+		Write-Host "Error: you need to execute this command from the folder where you downloaded it"
+	}
+}
+
 
 function Launch-Nvim {
 	$command = "`"c:\program files\Neovim\bin\nvim.exe`""
@@ -68,9 +98,13 @@ function ProfileEdit {
 	& notepad.exe $profile
 }
 
+Set-Alias -Name pinstall -Value Profile-Install -Description "Get Install Instructions"
 Set-Alias -Name hed -Value Admin-Edit-Hosts -Description "Edit hosts file in admin mode"
 Set-Alias -Name her -Value Admin-Run-HostEdit -Description "Launch hostedit in admin mode"
 Set-Alias -Name vi -Value Launch-Nvim -Description "Launch neovim"
+Set-Alias -Name vim -Value Launch-Nvim -Description "Launch neovim"
+Set-Alias -Name nvim -Value Launch-Nvim -Description "Launch neovim"
+Set-Alias -Name npp -Value Launch-NotepadPlusPlus
 Set-Alias -Name np -Value Launch-NotepadPlusPlus
 #Set-Alias -Name cdh -Value Alias-cdh -Description "Alias cdh: go to current user home directory"
 Set-Alias -Name cdh -Value Alias-cdh -Description "cd to current user home folder" 
--- psprofile.md
+++ psprofile.md
@@ -1,3 +1,23 @@
 # psprofile
+
 a tool to manage powershell aliases
+Marco Guardigli, october 2023
+
+Allows team definition and management of powershell aliases to automate tasks,
+for windows users.
+
+## Installation
+Open a powershell prompt and, from your home folder, execute the following commands
+```
+    mkdir psprofile
+    cd psprofile
+    git clone https://github.com/mgua/psprofile.git
+    . .\profile.ps1
+```
+
+Check the code for the defined aliases, and define yours
+once done, put your profile.ps1 in your Documents folder
+and it will be automatically loaded by powershell once you open a prompt
+
+
 
